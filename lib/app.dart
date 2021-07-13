8eddcd749c586b48a43373bb284568eaa5ee8433
Lucas Tamir Faria <lucastamir@gmail.com>
2021-07-13 18:49:45 -0300

    feat: criação da classe MyApp

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'modules/pages/home_page.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "API Request",
+      home: HomePage(),
+    );
+  }
+}
